8fff08b5046a18aa0bab51c040a80777fb45d969
himehowareu <himehowareu@gmail.com>
2022-07-13 09:04:15 -0500

    split up main function

--- cvm.py
+++ cvm.py
@@ -1,3 +1,4 @@
+import argparse
 from filters import passes
 from frames import frameData
 from func import runs
@@ -5,14 +6,22 @@ import Data
 from helper import log, debugCommand
 
 
-def main(programFile: str = "example.cvm"):
-    """
-    Takes a file to parse, filters and then runs the code
+def runToken(token: str):
+    if token.startswith("!"):
+        debugCommand(token)
+    elif not runs(token):
+        data = frameData(token)
+        Data.FrameStack.append(data)
 
-    :param programFile: The file to run, defaults to example.cvm
-    :type programFile: str (optional)
+
+def loadFile(fileName: str):
     """
-    with open(programFile, "r") as file:
+    Takes a file to parse, filters and load to the stack
+
+    :param fileName: The file to load
+    :type fileName: str (optional)
+    """
+    with open(fileName, "r") as file:
         code: str = file.read()
 
     for filter in passes:
@@ -20,24 +29,34 @@ def main(programFile: str = "example.cvm"):
         code = filter[0](code)
 
     Data.CodeStack.extend(code[::-1])
+
+
+def runFile(programFile: str = "example.cvm"):
+    """
+    Takes a file to parse, filters and then runs the code
+
+    :param programFile: The file to run, defaults to example.cvm
+    :type programFile: str (optional)
+    """
+    loadFile(programFile)
+
     Data.FunctionStack = []
 
     while len(Data.CodeStack):
-        # debugCommand("!dump")
         token = Data.CodeStack.pop()
         log("token: ", token)
-        if token.startswith("!"):
-            debugCommand(token)
-            continue
-        if not runs(token):
-            data = frameData(token)
-            Data.FrameStack.append(data)
-
-        log("stack:", Data.FrameStack)
+        runToken(token)
 
     if len(Data.FrameStack):
         print("Stack not empty: unused value")
 
 
+parser = argparse.ArgumentParser(prog="CVM", description="CVM programing language")
+parser.add_argument("file", help="the program you want to run", default="test.cvm")
+# parser.add_argument(
+#     "-command", metavar="c", nargs="+", help="run some commands from a string"
+# )
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    main("test.cvm")
+    runFile(args.file)
